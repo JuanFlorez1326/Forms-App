050f53471f9f4a0b0f63932a1881bf2296616bc3
Juan Florez <jpatino@latinia.com>
2023-11-19 19:49:05 -0500

    NEW: Dynamics Form

--- src/app/reactive/components/dynamics/dynamics.component.ts
+++ src/app/reactive/components/dynamics/dynamics.component.ts
@@ -1,11 +1,59 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamics',
-  templateUrl: './dynamics.component.html',
-  styles: [
-  ]
+  templateUrl: './dynamics.component.html'
 })
 export class DynamicsComponent {
 
+  constructor ( private fb: FormBuilder ) {}
+
+  public formGroup: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required]
+    ])
+  });
+
+  public onSubmit(): void {
+    if (this.formGroup.invalid) {
+      this.formGroup.markAllAsTouched();
+      return;
+    }
+    console.log(this.formGroup.value);
+    this.formGroup.reset();
+  }
+  
+  get favoriteGamesControl() {
+    return this.formGroup.controls['favoriteGames'] as FormArray
+  }
+
+  public isValidField( field: string ): boolean | null {
+    return this.formGroup.controls[field].errors
+      && this.formGroup.controls[field].touched;
+  }
+
+  isValidFieldInArray( formArray: FormArray, index: number ) {
+    return formArray.controls[index].errors
+      && formArray.controls[index].touched;
+  }
+
+  public getFieldError( field: string ): string | null {
+
+    if (!this.formGroup.controls[field]) return null;
+    const errors = this.formGroup.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido.';
+        case 'minlength':
+          return `Minimo ${ errors['minlength'].requiredLength } caracteres.` ;
+      } 
+    }
+
+    return null;
+  }
 }
