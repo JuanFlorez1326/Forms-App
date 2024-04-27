d48139b615de5790df3e9776f2871d596084bc21
Juan Forez <juanks339@gmail.com>
2024-04-27 16:04:29 -0500

    NEW: Validators functions

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,7 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from 'src/app/shared/validators/validators.helpers';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html'
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+  public myForm: FormGroup =  this.fb.group({
+    name: ['', [ Validators.required ]],
+    email: ['', [ Validators.required ]],
+    username: ['', [ Validators.required, cantBeStrider ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required ]]
+  })
+
+  public isValidField(field: string) {}
+
+  public onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
+}
