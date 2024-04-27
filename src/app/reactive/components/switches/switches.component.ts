71bcd75d77098d9b8a819b75fc5f1088780bc2bb
Juan Forez <82609508+JuanFlorez1326@users.noreply.github.com>
2024-04-27 15:29:03 -0500

    Validatiosn Switches

--- src/app/reactive/components/switches/switches.component.ts
+++ src/app/reactive/components/switches/switches.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -6,6 +7,43 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class SwitchesComponent {
+export class SwitchesComponent implements OnInit {
 
+  public person = {
+    gender: 'F',
+    wantNotifications: false
+  };
+
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.myForm.reset(this.person);
+  }
+ 
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue]
+  })
+
+  public isValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+
+  public onSave() {
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+
+    this.person = newPerson;
+    console.log(this.myForm.value);
+    console.log(this.person);
+  }
 }
